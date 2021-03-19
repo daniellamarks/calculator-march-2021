04cd2aefcf1ca6254029a9603ff792968545aa0b
Daniella Marks <marks.daniella@gmail.com>
2021-03-18 17:53:11 -0700

    testing, added functinos, to utils.js

--- app.js
+++ app.js
@@ -1,65 +1,39 @@
 // import functions and grab DOM elements
+import { add, subtract, multiply, divide } from ./utils.js
 
 const input1 = document.getElementById('input-1');
 const input2 = document.getElementById('input-2');
-
 const addButton = document.getElementById('add-button');
 const subtractButton = document.getElementById('subtract-button');
 const multiplyButton = document.getElementById('multiply-button');
 const divideButton = document.getElementById('divide-button');
-
 const display1 = document.getElementById('answer');
-
 
 addButton.addEventListener('click', () => {
     const value1 = input1.value;
     const value2 = input2.value;
-
-    console.log(value1, value2);
-
-    const sum = Number(value1) + Number(value2);
-
-    console.log(sum);
-
+    const sum = add(Number(value1), Number(value2))
     display1.textContent = sum;    
 });
 
 subtractButton.addEventListener('click', () => {
     const value1 = input1.value;
     const value2 = input2.value;
-
-    console.log(value1, value2);
-
-    const difference = Number(value1) - Number(value2);
-
-    console.log(difference);
-
+    const difference = subtract(Number(value1), Number(value2))
     display1.textContent = difference;    
 });
 
 multiplyButton.addEventListener('click', () => {
     const value1 = input1.value;
     const value2 = input2.value;
-
-    console.log(value1, value2);
-
-    const product = Number(value1) * Number(value2);
-
-    console.log(product);
-
+    const product = multiply(Number(value1),Number(value2))
     display1.textContent = product;    
 });
 
 divideButton.addEventListener('click', () => {
     const value1 = input1.value;
     const value2 = input2.value;
-
-    console.log(value1, value2);
-
-    const quotient = Number(value1) / Number(value2);
-
-    console.log(quotient);
-
+    const quotient = divide(Number(value1), Number(value2))
     display1.textContent = quotient;    
 });
 
@@ -70,6 +44,3 @@ clearButton.addEventListener('click', () => {
     input2.value = '';
     display1.textContent = '';
 });
-// initialize state
-
-// set event listeners to update state and DOM
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,59 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { add, subtract, multiply, divide } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should return 7 if I pass it 3 and 4', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 7;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add(3, 4);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('subtract should return 5 if I pass it 10 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 5;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(10, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('multiply should return 50 if I pass it 10 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 50;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(10, 5);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('subtract should return 2 if I pass it 10 and 5', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 2;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divide(10, 5);
 
     //Expect
     // Make assertions about what is expected versus the actual result
